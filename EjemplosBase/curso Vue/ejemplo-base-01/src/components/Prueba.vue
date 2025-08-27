3ec08974dae99eb6b52d58b7868e7422054096da
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2025-08-27 17:29:51 -0300

    Re-estructuración del contenido del Repositorio

---
+++ EjemplosBase/curso Vue/ejemplo-base-01/src/components/Prueba.vue
@@ -0,0 +1,11 @@
+<script setup>
+    defineProps({
+        color:String
+    })
+</script>
+<template>
+    <div class="w-full h-1/3 "></div>
+</template>
+<style scoped>
+    @import "tailwindcss";
+</style>

6a7b11371b746886c8da61308da528f91f853111
Sebastián de los Ángeles <luis.delos@ddocente.ceibal.edu.uy>
2025-09-13 22:56:11 -0300

    Registro.vue: se consigue registrar un usuario en el backend. Se avanza en el análisis de errores y se crea una clase CSS (.error) para resaltar campos erróneos

--- EjemplosBase/notas-base/src/views/Registro.vue
+++ EjemplosBase/notas-base/src/views/Registro.vue
@@ -1,3 +1,34 @@
+<script setup>
+    import { ref } from 'vue';
+    import instanciaAxios from '../services/api';
+
+    const nombreUsuario = ref('')
+    const email = ref('')
+    const password = ref('')
+    const password_confirmation = ref('')
+
+    const enviarDatos = async ()=>{
+        const nUsuario = {
+            name:nombreUsuario.value,
+            email:email.value,
+            password:password.value,
+            password_confirmation:password_confirmation.value
+        }
+
+        try {
+            const respuesta = await instanciaAxios.post('usuario/nuevo', nUsuario)
+            console.log("RESPUESTA -->") 
+            console.log(respuesta.data)
+            alert("El servidor dice: " + respuesta.data.estado + "\n" + respuesta.data.mensaje)
+        } catch (error) {
+            console.error("ERROR --> ")
+            console.error(error)
+            console.error(error.response.data)
+        }
+    }
+
+</script>
+
 <template>
     <div class="base 
         w-[100vw] h-[80vh]
@@ -5,10 +36,11 @@
         flex justify-center items-center  
         ">
         <div class="contenido
-                w-4/5 h-4/5
-                bg-green-500
+                w-2/5 h-4/5
+                bg-green-300
                 rounded-3xl
                 p-2
+                flex flex-col items-center
                 text-center
             ">
             <h1 class="titulo
@@ -17,32 +49,45 @@
                 Registrarse en el Sistema
             </h1>
             <br>
-            <form class="formRegistro" @submit.prevent >
-                <label for="">Nombre de usuario:</label>
-                <input type="text">
-                <br>
-                <label for=""></label>
-                <input type="text">
-                <br>
-                <label for=""></label>
-                <input type="text">
-                <br>
-                <label for=""></label>
-                <input type="text">
+            <form @submit.prevent="enviarDatos" class="formRegistro
+                                    relative top-10
+                                    w-4/5
+                                    grid grid-cols-2 grid-rows-4 gap-1
+                                    text-left      
+                ">
+                <label for="nombreUsuario">Nombre de usuario:</label>
+                <input type="text" id="nombreUsuario" v-model="nombreUsuario">
+
+                <label for="email">Email:</label>
+                <input type="text" id="email" v-model="email">
+
+                <label for="password">Contraseña:</label>
+                <input type="password" id="password" v-model="password" required>
+
+                <label for="password_confirmation">Confirmar Contraseña:</label>
+                <input type="password" id="password_confirmation" v-model="password_confirmation" required>
+                <p class="h-4"></p>
+                <button class="btnRegistro
+                            col-span-2
+                            bg-amber-50
+                    ">
+                    Crear Usuario
+                </button>
             </form>
         </div>
         
     </div>
 </template>
-<script>
-export default {
-    
-}
-</script>
+
 <style scoped>
     .formRegistro input{
         margin-left: 20px;
         background-color: white;
+    }
+
+    .error {
+        border: solid red 1px;
+        background-color: pink;
     }
     
 </style>

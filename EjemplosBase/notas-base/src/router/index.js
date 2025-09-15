d116f51516268ff281d3d96da532555ddf0964f5
Sebastián de los Ángeles <luis.delos@ddocente.ceibal.edu.uy>
2025-09-14 23:17:47 -0300

    Se realizan modificaciones para asegurar que se utiliza JWT, Se debe profundizar en su uso

--- EjemplosBase/notas-base/src/router/index.js
+++ EjemplosBase/notas-base/src/router/index.js
@@ -9,7 +9,7 @@ const router = createRouter(
         history: createWebHistory(import.meta.env.BASE_URL),
         routes: [
             {
-                path: "/",
+                path: "/notas",
                 component: Inicio,
                 name: 'inicio'
             }, 
--- EjemplosBase/notas-base/src/views/InicioSesion.vue
+++ EjemplosBase/notas-base/src/views/InicioSesion.vue
@@ -1,14 +1,87 @@
+<script setup>
+    import { ref } from 'vue';
+    import router from '../router';
+
+    import instanciaAxios from '../services/api';
+
+    const email = ref('')
+    const password = ref('')
+
+    const enviarDatos = async ()=>{
+        const datosUsuario = {
+            email:email.value,
+            password:password.value,
+        }
+
+        try {
+            const respuesta = await instanciaAxios.post('ingresar', datosUsuario)
+            console.log("RESPUESTA -->") 
+            console.log(respuesta.data)
+            alert("El servidor dice: " + respuesta.data.estado + "\n" + respuesta.data.mensaje)
+            let destino = respuesta.data.destino
+            router.push(destino)
+
+        } catch (error) {
+            console.error("ERROR --> ")
+            console.error(error)
+            console.error(error.response.data)
+        }
+    }
+</script>
+
 <template>
     <div class="base 
-            w-[100vw] h-full
-            bg-amber-300    
+            w-[100vw] h-[80vh]
+            bg-amber-300
+            flex justify-center items-center  
         ">
-        
+        <div class="contenido
+                w-2/5 h-4/5
+                bg-green-300
+                rounded-3xl
+                p-2
+                flex flex-col items-center
+                text-center
+            ">
+            <h1 class="titulo
+                    text-4xl font-bold
+                ">
+                Ingresar en el Sistema
+            </h1>
+            <br>
+            <form @submit.prevent="enviarDatos" class="formLogin
+                                    relative top-10
+                                    w-4/5
+                                    grid grid-cols-2 grid-rows-4 gap-1
+                                    text-left      
+                ">
+
+                <label for="email">Email:</label>
+                <input type="text" id="email" v-model="email">
+
+                <label for="password">Contraseña:</label>
+                <input type="password" id="password" v-model="password" required>
+                <p class="h-4"></p>
+                <button class="btnRegistro
+                            col-span-2
+                            bg-amber-50
+                    ">
+                    Crear Usuario
+                </button>
+            </form>
+        </div>
     </div>
 </template>
-<script setup>
 
-</script>
 <style scoped>
+    .formLogin input{
+        margin-left: 20px;
+        background-color: white;
+    }
+
+    .error {
+        border: solid red 1px;
+        background-color: pink;
+    }
     
 </style>

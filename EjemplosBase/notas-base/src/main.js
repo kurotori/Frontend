a4775a801833eade735628f08eef233ee86a5d9b
luissebastianangeles@gmail.com <Sebastin de los ngeles>
2025-09-17 23:38:12 -0300

    CAmbios menores en main.js

--- EjemplosBase/notas-base/src/main.js
+++ EjemplosBase/notas-base/src/main.js
@@ -12,9 +12,6 @@ import instanciaAxios from "./services/api";//AXIOS: Se agrega axios al proyecto
 const pinia = createPinia();
 const app = createApp(App);
 
-
-const pinia = createPinia(); //PINIA: Se crea la instancia de pinia
-
 //AXIOS: configuración
 /* axios.defaults.baseURL = 'http://127.0.0.1:8000/api/' // AXIOS: Se establece la URL base a la que apunta axios
 axios.defaults.headers.post['Content-Type'] = 'application/json' //AXIOS: configuración de cabeceras 
